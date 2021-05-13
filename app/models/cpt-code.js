dfff5dde9363715f7c8452b8c4b9925d00b4b925
Ramiro Franco <me@ramiro.mx>
2021-05-13 15:24:09 -0700

    Definting cpt code model

--- app/models/cpt-code.js
+++ app/models/cpt-code.js
@@ -1,5 +1,8 @@
-import Model from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class CptCodeModel extends Model {
-
+  @attr description;
+  @attr duration;
+  @attr rate;
+  @attr callToBook;
 }
