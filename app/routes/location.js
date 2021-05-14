92e769838017e0928ea447aa03a790bd0f3375a2
Ramiro Franco <me@ramiro.mx>
2021-05-14 00:58:49 -0700

    Make locations route ... routable

--- app/routes/location.js
+++ app/routes/location.js
@@ -1,4 +1,12 @@
+import ENV from 'sp-test/config/environment';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class LocationRoute extends Route {
+  @service store;
+
+  model({ service_id: cptCodeId }) {
+    let { clinicianId } = ENV.APP;
+    return this.store.query('office', { filter: { clinicianId, cptCodeId } });
+  }
 }
