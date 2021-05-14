be7fba584fa9f819601c38bae57fb85a126a67b2
Ramiro Franco <me@ramiro.mx>
2021-05-14 04:10:38 -0700

    Add service option support in left panel, and styling

--- app/controllers/location.js
+++ app/controllers/location.js
@@ -2,6 +2,19 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class LocationController extends Controller {
+  get service() {
+    let { service_id } = this.target.currentRoute.params;
+    return this.model.services.findBy('id', service_id);
+  }
+
+  get serviceDuration() {
+    return this.service.duration;
+  }
+
+  get serviceType() {
+    return this.service.description;
+  }
+
   @action
   select() {
     alert('Ideally, this would move you toward the next step of the process.');
